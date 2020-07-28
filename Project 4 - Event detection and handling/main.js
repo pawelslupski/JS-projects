1595fb34611c60a29d7e9ee63bf46d3233337c89
pawelslupski <pawelslupski.go@gmail.com>
2020-07-28 23:28:28 +0200

    changing the background color when moving the mouse & code optimization

--- Project 4 - Event detection and handling/main.js
+++ Project 4 - Event detection and handling/main.js
@@ -1,6 +1,15 @@
 const h1 = document.querySelector('h1');
 
 document.body.addEventListener('mousemove', function(e) {
-    // console.log(e.clientX, e.clientY);
-    h1.textContent = `${e.clientX}, ${e.clientY}`;
+    const x = e.clientX;
+    const y = e.clientY;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    h1.textContent = `${x}, ${y}`;
+
+    const red = x / width * 255;
+    const green = y / height * 255;
+    const blue = ((x / width * 255) + (y / height * 255)) / 2;
+
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${red / green})`;
 })
