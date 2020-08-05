52717c2c301d08472f4c0ba3a7f2f8e842b48013
pawelslupski <pawelslupski.go@gmail.com>
2020-08-05 20:55:07 +0200

    Manual slide changing applied

--- Project 9 - Banner/main.js
+++ Project 9 - Banner/main.js
@@ -14,10 +14,18 @@ const slideList = [{
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')];
 
 // Interface
 const time = 3000;
 let active = 0;
+
+// Implementation
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
 
 const changeSlide = () => {
     active++;
@@ -26,8 +34,26 @@ const changeSlide = () => {
     }
     image.src = slideList[active].img;
     h1.textContent = slideList[active].text;
+    changeDot();
 }
 
-setInterval(changeSlide, time);
+let indexInterval = setInterval(changeSlide, time);
 
-// Implementation
+const keyChangeSlide = (e) => {
+    if (e.keyCode == 37 || e.keyCode == 39) {
+        clearInterval(indexInterval);
+        e.keyCode == 37 ? active-- : active++;
+        if (active === slideList.length) {
+            active = 0;
+        } else if (active < 0) {
+            active = slideList.length - 1;
+        }
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot();
+    indexInterval = setInterval(changeSlide, time);
+}
+
+// utwórz funkcję keyChangeSlide. Zadanie może wymagać także zmian poza funkcją
+window.addEventListener('keydown', keyChangeSlide);
